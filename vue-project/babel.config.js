89c4b27f13a3a40fc84ba268b7eef58659c51bbe
devsunset <devsunset@gmail.com>
2021-06-21 16:21:04 +0900

    dev

--- vue-project/babel.config.js
+++ vue-project/babel.config.js
@@ -2,4 +2,13 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
-} 
+}
+
+/*
+// prefetch delete
+module.exports = {
+  chainWebpack : config => {
+    config.plugins.delete('prefetch); 
+  }
+}
+*/
