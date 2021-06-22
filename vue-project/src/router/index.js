1d2f143a661330c620c8223b55576ae5b29b63fa
devsunset <devsunset@gmail.com>
2021-06-22 11:23:12 +0900

    dev

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -8,23 +8,17 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
   // {
   //   path: '/example',
   //   name: 'Example',
   //   component: Example
-
   // }
   {
     path: '/example',
     name: 'Example',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "example" */ '../views/Example.vue')
   },
   {
@@ -47,7 +41,6 @@ const routes = [
     name: 'GoogleLogin',
     component: () => import( /* webpackChunkName: "googlelogin" */ '../views/GoogleLogin.vue')
   }
-
 ]
 
 const router = createRouter({
