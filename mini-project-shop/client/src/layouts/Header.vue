ad67094332bac7068fa77ab203a7efd0b2a9e0e8
devsunset <devsunset@gmail.com>
2021-06-23 19:41:43 +0900

    dev

--- mini-project-shop/client/src/layouts/Header.vue
+++ mini-project-shop/client/src/layouts/Header.vue
@@ -20,8 +20,7 @@
             <router-link class="nav-link" to="/sales">제품등록페이지</router-link>
           </li>
           <li v-if="user.email==undefined"><button class="btn btn-danger" type="button" @click="kakaoLoin">로그인</button></li>
-          <li v-else><button class="btn btn-danger" type="button" @click="ls
-          ">로그아웃</button></li>
+          <li v-else><button class="btn btn-danger" type="button" @click="kakaoLogout">로그아웃</button></li>
         </ul>
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
